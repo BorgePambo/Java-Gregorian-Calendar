d223e9734ff204d7525c84115fa9bad9d3349863
BorgePambo <borginhopambo01@gmail.com>
2021-07-04 14:35:20 -0300

    Gregorian Calendar

---
+++ src/javaappgregoriancalendar/JavaAppGregorianCalendar.java
@@ -0,0 +1,17 @@
+package javaappgregoriancalendar;
+
+import java.util.GregorianCalendar;
+
+
+public class JavaAppGregorianCalendar {
+
+   
+    public static void main(String[] args) {
+        
+         GregorianCalendar data = new GregorianCalendar(2020,10,5);
+         
+         System.out.println(data.getTime());
+                
+    }
+    
+}
